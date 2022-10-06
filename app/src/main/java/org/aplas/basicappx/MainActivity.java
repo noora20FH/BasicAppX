3a7f1e12c9372126eef43256710320b2715733ce
noora20FH <noora20fairy@gmail.com>
2022-10-06 19:24:12 +0700

    Initial commit

---
+++ app/src/main/java/org/aplas/basicappx/MainActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.basicappx;
+
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
